6f28ae586b1233653595a615897cd0b47b6289d3
Gab27x <gabrielernestoeb@outlook.com>
2025-06-15 22:53:29 -0500

    query all implemented

--- queryServer/src/main/java/QueryServer.java
+++ queryServer/src/main/java/QueryServer.java
@@ -1,37 +1,58 @@
-import com.zaxxer.hikari.HikariDataSource;
+
 import com.zeroc.Ice.Communicator;
 import com.zeroc.Ice.Current;
 import com.zeroc.Ice.ObjectAdapter;
 import com.zeroc.Ice.Util;
 import java.sql.Connection;
-import java.sql.SQLException;
+
 
 
 public class QueryServer implements Query.QueryServerI {
 
+	private DAO dao;
+
+	public QueryServer(DAO dao) {
+		this.dao = dao;
+	}
 
 	@Override
 	public String getVotingTableById(String id, Current current) {
 		System.out.println("query from: " + id);
-		if (id.equalsIgnoreCase("1"))
-			return "vaya vote a icesi";
-		else
-			return "no esta registrado";
+		try {
+			// aquí podrías parsear el id si es necesario
+			int idInt = Integer.parseInt(id);
+			String resultado = dao.obtenerInfoCiudadanoPorId(idInt);
+			return resultado != null ? resultado : "no encontrado";
+		} catch (NumberFormatException e) {
+			return "ID inválido";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Error al consultar";
+		}
 	}
 
 	public static void main(String[] args) {
-
 		String cfg = args.length > 0 ? args[0] : "QueryServer.cfg";
-		try (Communicator communicator = Util.initialize(args, cfg)) {
-			QueryServer server = new QueryServer();
+
+		try (
+			Connection conexion = ConexionDB.conectar();
+			Communicator communicator = Util.initialize(args, cfg)
+		) {
+			DAO dao = new DAO(conexion);
+			QueryServer server = new QueryServer(dao);
 
 			ObjectAdapter adapter = communicator.createObjectAdapter("QueryServer");
 			adapter.add(server, Util.stringToIdentity("QueryServer"));
 			adapter.activate();
 			System.out.println("QueryServer up ...");
 
+
 			communicator.waitForShutdown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConexionDB.cerrarPool(); // si usas pool
 		}
 	}
-
 }
+
